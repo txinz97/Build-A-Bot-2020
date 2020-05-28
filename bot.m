7f427dd57be92bed043b4313cedac74db303a1d7
txinz97 <65388923+txinz97@users.noreply.github.com>
2020-05-28 17:17:00 +0800

    Update bot.m
    added buttons for catch-all

--- bot.m
+++ bot.m
@@ -7,24 +7,20 @@
 \ @: practice
 \ practice: practice practices
 
+
 Q: practices
 A: Ok let's see some practices! ${ "Gogo!" button }
 K: $back practices
-
 --
-
 Q: quiz
 A: OK, let's try the quiZ! ${ "Gogo!" button }
 K: $back quiz
-
 --
-
 Q: resources
 A: OK, lets check out some resources! ${ "Gogo!" button }
 K: $back resources
-
 --
 Q: $_
-A: There are things a guru don't know too, you know.
+A: Let's try something else then! ${ "Environmental Practices" button } ${ "Environmental Quiz" button } ${ "Environmental Resources" button }
 L: ${ last-question }
 --
