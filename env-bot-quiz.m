b205360950ec3a1c060086c81952fc89f170302d
txinz97 <65388923+txinz97@users.noreply.github.com>
2020-05-28 22:36:17 +0800

    Update env-bot-quiz.m

--- env-bot-quiz.m
+++ env-bot-quiz.m
@@ -16,46 +16,46 @@
 room: quiz
 
 Q: Gogo
-A: Alright, let's do this! Just answer these questions... to make it easy it's MCQ :) ${ "OK" button } ${ "No" button }
+A: Alright, let's do this! Just answer these questions... to make it easy it's MCQ :) <br> ${ "OK" button } ${ "No" button }
 --
 Q: no
-A: Alright, what would you like to discuss instead? ${ "Environmental Practices" button } ${ "Environmental Quiz" button } ${ "Environmental Resources" button }
+A: Alright, what would you like to discuss instead? <br> ${ "Environmental Practices" button } ${ "Environmental Quiz" button } ${ "Environmental Resources" button }
 --
 Q: OK
-A: What is a green way to use kitchen scraps? ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
+A: What is a green way to use kitchen scraps? <br> ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
 --
 Q: Eat|Throw
-A: Hmmm... not quite! Try another answer! ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
+A: Hmmm... not quite! Try another answer! <br> ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
 --
 Q: Compost
 A: That's right! Once it decomposes, it can become a nutritious fertilizer! Otherwise it gets dumped into polluting landfills :( ${ ">>>>" button }
 --
 Q: >>>>
-A: What's the most sustainable activity you can do for mother Earth? ${ "Activism" button } ${ "Reduce Food Waste" button } ${ "Reuse Shopping Bags" button }
+A: What's the most sustainable activity you can do for mother Earth? <br> ${ "Activism" button } ${ "Reduce Food Waste" button } ${ "Reuse Shopping Bags" button }
 --
 Q: Activism|Food|Shopping
 A: Yes! In fact, all solutions are good. There are so many ways we can be green, every little action matters! ${ ">>>" button }
 --
 Q: >>>
-A: What is sustainability? ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button }
+A: What is sustainability? <br> ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button }
 --
 Q: Maintaining at a certain rate or level
-A: Good job! In fact, the principles of sustainability are: profit, people and planet. ${ ">>" button }
+A: Good job! In fact, the principles of sustainability are: profit, people and planet. <br> ${ ">>" button }
 --
 Q: Eating|Awake
-A: Not quite, try again! ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button } 
+A: Not quite, try again! <br> ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button } 
 --
 Q: >>
-A: Which is the best possible option in living sustainably? ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
+A: Which is the best possible option in living sustainably? <br> ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
 --
 Q: Refuse
 A: Amazing! Indeed, refusing to use or support products that harm the environment is the most ideal way to live sustainably. ${ ">" button }
 --
 Q: Reduce|Reuse
-A: That is indeed a good option, but there is a better one! Try again! ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
+A: That is indeed a good option, but there is a better one! Try again! <br> ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
 --
 Q: >
-A: All right, that's all the quizzes for today! You now know enough to become a green warrior! While you're at it, here's a website to calculate your carbon footprint to learn more: https://climatehero.me/
+A: All right, that's all the quizzes for today! You now know enough to become a green warrior! While you're at it, here's a website to calculate your carbon footprint to learn more: https://climatehero.me/ <br><br> Now let's try something else! <br> ${ "Environmental Practices" button }  ${ "Environmental Resources" button }
 
 \ Include a badge here if possible.
 
