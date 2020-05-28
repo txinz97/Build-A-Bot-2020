67837045031716de1bef7a24fc9baffdcbb1e65c
txinz97 <65388923+txinz97@users.noreply.github.com>
2020-05-28 23:54:51 +0800

    Update env-bot-practices.m

--- env-bot-practices.m
+++ env-bot-practices.m
@@ -5,27 +5,29 @@
 \ A brief description of this program:
 \ This code will show some good environmental practices.
 
+
 \ 1. Formatting
 @: random specific consumption habit bigger_thing
 \ eat drink
-
-random: anything random any
-
+random: anything random any another
 specific: specific
 consumption: personal_consumption consumption personal
 habit: little habit little_habit
 bigger_thing: bigger thing bigger_thing
-\ eat: food eat meal 
-\ drink: drinks
 
-\ search: random_list
-\ eat_list drink_list
+
+
+
 assoc: random_list image
-\ ids title author
+
+
+
 mem: count
-\ id
+
+
 
 {{
+
     1 "Monitor and manage your consumption (clothes, food)!"
     2 "Watch your electricity and water use!"
     3 "Support local products! Having to ship goods over shorter distances reduce the carbon emissions from transport."
@@ -34,9 +36,12 @@ mem: count
     6 "Try shopping at zero-waste stores!"
     7 "Join environment groups!"
     8 "Educate people around you!"
+
 }} +random_list
 
+
 {{
+
     1 "https://upload.wikimedia.org/wikipedia/commons/3/37/Consumption_%28sign%29.jpg"
     2 "https://upload.wikimedia.org/wikipedia/commons/8/84/Los_Olivitos_-_estado_Zulia.jpg"
     3 "https://live.staticflickr.com/4378/36338134252_5ecf19415b_b.jpg"
@@ -45,38 +50,47 @@ mem: count
     6 "https://thesmartlocal.com/wp-content/uploads/2019/06/images_easyblog_articles_7921_Zero-Waste-stores-in-Singapore-23-1.jpg"
     7 "https://www.todayonline.com/sites/default/files/styles/new_app_article_detail/public/photos/43_images/yjrally07.jpg?itok=IcA-By2i"
     8 "https://outdooreducationaustralia.org.au/wp-content/uploads/Outdoor-Ed-Teenagers-in-circle.jpg"
+
 }} +image
+
 
 \ 2. Q&A list, Rooms
 
+
 room: practices
 
-Q: Gogo
+Q: Gogo|Restart
 A: Do you want to see a specific practice? Or just a random one? ${ "Random" button } ${ "Specific" button }
 --
-
 Q: $x.@random
-A: ${ count random_list } ${ count image }
-K: +count %
+A: count 8 < % ${ count random_list } ${ count image } ${ "Another" button }
+A: count 8 >= % ${ 8 random_list } ${ 8 image } ${ "Restart" button }
+K: count 8 >= % $back practices
+K: count+ %
 --
-
-\ Currently, you can't add buttons with two words init (someone else on the group is having the same problem). still tryna figure it out.
-\ in the meantime, i used the following buttons:
 Q: $x.@specific
-A: What kind of $x ? ${ "Consumption" button } ${ "Habits" button } ${ "Bigger_things" button }
+A: What kind of $x ? ${ "Consumption" button } ${ "Habits" button } ${ "Bigger things" button }
+
 --
-Q: $consumption|$consumption+s
-A: ${ 1 random_list } ${ 1 image }; ${ 2 random_list } ${ 2 image }
+Q: $x.@consumption
+A: ${ 1 random_list } ${ 1 image } ${ "Another" button }; ${ 2 random_list } ${ 2 image } <br> ${ "Another" button }
+
 --
-Q: $habit|$habit+s
-A: ${ 3 random_list } ${ 3 image }; ${ 4 random_list } ${ 4 image }; ${ 5 random_list } ${ 5 image }; ${ 6 random_list } ${ 6 image }
+Q: $habit|$habit+s|$random
+A: ${ 3 random_list } ${ 3 image } ${ "Another" button }; ${ 4 random_list } ${ 4 image } ${ "Another" button }; ${ 5 random_list } ${ 5 image } ${ "Another" button }; ${ 6 random_list } ${ 6 image } ${ "Another" button }
 --
 Q: $bigger_thing|$bigger_thing+s
-A: ${ 7 random_list } ${ 7 image }; ${ 8 random_list } ${ 8 image }
+A: ${ 7 random_list } ${ 7 image } ${ "Another" button } 
 --
+
 \ ### TO PUT IN THE bot.m file after all micro-topics are done!
+
 \ Q: $x.@eat
+
 \ $x eat_list ids id! % ${ id title } by ${ id author } ${ id image }
+
 \ --
+
+
 
 end-room
