f8132dad608d91cffb53d01be130bba0d22edfa7
txinz97 <65388923+txinz97@users.noreply.github.com>
2020-05-28 17:40:39 +0800

    Update env-bot-quiz.m
    added new questions and cleaned up code (removed unnecessary types and things)
    
    avoid repeating the words: activism, food, shopping, eating, awake, reduce, reuse

--- env-bot-quiz.m
+++ env-bot-quiz.m
@@ -1,31 +1,15 @@
 \
+
 \ @author: c6z3h001
 \ @date: 28 May 2020
 \
 \ A brief description of this program:
 \ This code will show some good environmental practices.
 
+
 \ 1. Formatting
-@: Eat_it Throw_it Activism reduce_fw reuse_sb
-Eat_it: Eat_it
-Throw_it: Throw_it
-Activism: Activism
-reduce_fw: Reduce_Food_Waste
-reuse_sb: Reuse_Shopping_Bags
-\ random specific consumption habit bigger_thing
-\ random: anything random any
 
-\ specific: specific
-\ consumption: personal_consumption consumption personal
-\ habit: little habit little_habit
-\ bigger_thing: bigger thing bigger_thing
 
-\ search: random_list
-\ eat_list drink_list
-\ assoc: random_list image
-\ ids title author
-\ mem: count
-\ id
 
 \ 2. Q&A list, Rooms
 
@@ -34,32 +18,49 @@ room: quiz
 Q: Gogo
 A: Alright, let's do this! Just answer these questions... to make it easy it's MCQ :) ${ "OK" button } ${ "No" button }
 --
-
 Q: no
-A: Alright, what would you like to discuss instead?
+A: Alright, what would you like to discuss instead? ${ "Environmental Practices" button } ${ "Environmental Quiz" button } ${ "Environmental Resources" button }
 --
-
 Q: OK
 A: What is a green way to use kitchen scraps? ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
 --
-
-Q: $Eat_it|$Throw_it
-A: Hmmm... not quite! Try another answer!
+Q: Eat|Throw
+A: Hmmm... not quite! Try another answer! ${ "Compost" button } ${ "Eat it" button } ${ "Throw it" button }
 --
-
 Q: Compost
-A: That's right! Once it decomposes, it can become a nutritious fertilizer! Otherwise it gets dumped into polluting landfills :( ${ "Next" button }
+A: That's right! Once it decomposes, it can become a nutritious fertilizer! Otherwise it gets dumped into polluting landfills :( ${ ">>>>" button }
 --
-
-Q: Next
+Q: >>>>
 A: What's the most sustainable activity you can do for mother Earth? ${ "Activism" button } ${ "Reduce Food Waste" button } ${ "Reuse Shopping Bags" button }
 --
-Q: $Activism|$reduce_fw|$reuse_sb
-A: Yes! In fact, all solutions are good. There are so many ways we can be green, every little action matters! ${ "Continue" button }
+Q: Activism|Food|Shopping
+A: Yes! In fact, all solutions are good. There are so many ways we can be green, every little action matters! ${ ">>>" button }
 --
-Q: Continue
+Q: >>>
+A: What is sustainability? ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button }
+--
+Q: Maintaining at a certain rate or level
+A: Good job! In fact, the principles of sustainability are: profit, people and planet. ${ ">>" button }
+--
+Q: Eating|Awake
+A: Not quite, try again! ${ "Maintaining at a certain rate or level" button } ${ "Living by eating" button } ${ "Staying awake" button } 
+--
+Q: >>
+A: Which is the best possible option in living sustainably? ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
+--
+Q: Refuse
+A: Amazing! Indeed, refusing to use or support products that harm the environment is the most ideal way to live sustainably. ${ ">" button }
+--
+Q: Reduce|Reuse
+A: That is indeed a good option, but there is a better one! Try again! ${ "Refuse" button } ${ "Reduce" button } ${ "Reuse" button }
+--
+Q: >
 A: All right, that's all the quizzes for today! You now know enough to become a green warrior! While you're at it, here's a website to calculate your carbon footprint to learn more: https://climatehero.me/
+
 \ Include a badge here if possible.
+
 --
+
+
 
 end-room
