308e930e09585c246d5d342f2b3dc4f068a93284
c6z3h <zchua019@e.ntu.edu.sg>
2020-05-28 16:28:14 +0800

    Create bot.m

---
+++ bot.m
@@ -0,0 +1,30 @@
+\
+\ @author: c6z3h001
+\ @date: 25 May 2020
+\
+\ A brief description of this program.
+\
+\ @: practice
+\ practice: practice practices
+
+Q: practices
+A: Ok let's see some practices! ${ "Gogo!" button }
+K: $back practices
+
+--
+
+Q: quiz
+A: OK, let's try the quiZ! ${ "Gogo!" button }
+K: $back quiz
+
+--
+
+Q: resources
+A: OK, lets check out some resources! ${ "Gogo!" button }
+K: $back resources
+
+--
+Q: $_
+A: There are things a guru don't know too, you know.
+L: ${ last-question }
+--
