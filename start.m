a7573b7936be45100de04ffc7a7b42df5dc088cf
Tay Xin Yi <65388923+txinz97@users.noreply.github.com>
2020-08-17 16:08:49 +0800

    Update start.m

--- start.m
+++ start.m
@@ -9,11 +9,11 @@
 start: start begin enter
 assoc: pic 
 {{
-   :start "Environmental Practices" button "Environmental Quiz" button concat 
-   "Environmental Resources" button concat
+   :start "Sustainability Practices" button "Sustainability Resources" button concat
+   "Sustainability Quiz" button concat "EnviroChat" button concat
 }} +pic
 
 
-Q: $x.@start
-A: What would you like to discuss? <br> ${ $x pic }
+Q: $x.@starts
+A: Welcome! You can learn more about environmentally sustainable practices, view resources, take a quiz to test your understanding or just chat with us! <br> Pick one https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Happy_smiley_face.png/20px-Happy_smiley_face.png <br><br> ${ $x pic }
 --
