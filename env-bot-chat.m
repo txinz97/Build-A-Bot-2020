b9c759670a54730ce5e4b59f51768c738ed5f3f4
Tay Xin Yi <65388923+txinz97@users.noreply.github.com>
2020-08-17 16:00:52 +0800

    Create env-bot-chat.m

---
+++ env-bot-chat.m
@@ -0,0 +1,39 @@
+\
+\ @author: txinz97
+\ @date: 29 May 2020
+\
+\ Chat portion of env bot.
+\ Includes simple greetings
+\
+room: EnviroChat
+
+@: how where what live sustainable
+how: how hwo
+where: where whr
+what: wat what wht
+live: live life living
+sustainable: sustainable sustainably sustainability
+
+Q: Gogo
+A: Hello! Ask me questions about: <br> <ul> <li> What is sustainability? </li> <li> What is living sustainably? </li> <li> How can we live sustainably? </li> </ul> <br> Let's chat! <br><br> Or would you like to try the other options? <br> ${ "Sustainability Practices" button }  ${ "Sustainability Resources" button } ${ "Sustainability Quiz" button }
+--
+Q: $what $sustainable
+Q: $what is $sustainable
+A: Generally, environmental sustainability is defined by the International Union for Conservation of Nature (IUCN) as the capacity to improve the quality of human life while living within the carrying capacity of the Earth’s supporting eco-systems. 
+--
+Q: $what $live $sustainable
+Q: $what $1 $live $sustainable
+Q: $what $1 $2 $live $sustainable
+A: Sustainable living usually includes changes to our lifestyles to reduce our usage of natural and personal resources. <br><br> This can include changing our energy consumption and mode of transport, minimizing our usage of plastic by using existing bags that we have, or repurposing an item for a different use instead of buying a new item. 
+--
+Q: $how $live $sustainable
+Q: $how $1 $live $sustainable
+Q: $how $1 $2 $live $sustainable
+Q: $how $1 $2 $3 $live $sustainable
+A: That's a great question! Some ways we can do this include: <br> <ul> <li> recycling items and packaging when possible, </li> <li> minimizing our use of single-use products, </li> <li> supporting brands that produce sustainably! </li> </ul>
+--
+Q: $where $sustainable
+Q: $sustainable $where
+A: You can practice sustainability habits almost anywhere: from the comfort of your own home, to zero-waste or local stores, at school/workplaces, and in public!
+--
+end-room
