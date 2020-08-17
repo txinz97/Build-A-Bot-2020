304305699273d28b82a7eeefeeda73ef0c3a4413
Tay Xin Yi <65388923+txinz97@users.noreply.github.com>
2020-08-17 16:00:21 +0800

    Update bot.m

--- bot.m
+++ bot.m
@@ -9,18 +9,22 @@
 
 
 Q: practices
-A: Ok let's see some practices! ${ "Gogo!" button }
+A: OK let's see some practices! ${ "Gogo!" button }
 K: $back practices
 --
 Q: quiz
-A: OK, let's try the quiZ! ${ "Gogo!" button }
+A: OK, let's try the quiz! ${ "Gogo!" button }
 K: $back quiz
 --
 Q: resources
-A: OK, lets check out some resources! ${ "Gogo!" button }
+A: OK, let's check out some resources! ${ "Gogo!" button }
 K: $back resources
 --
+Q: EnviroChat
+A: Sure, let's chat! ${ "Gogo!" button }
+K: $back EnviroChat
+--
 Q: $_
-A: Let's try something else then! ${ "Environmental Practices" button } ${ "Environmental Quiz" button } ${ "Environmental Resources" button }
+A: What would you like to know today? <br> ${ "Sustainability Practices" button } ${ "Sustainability Quiz" button } ${ "Sustainability Resources" button } ${ "EnviroChat" button }
 L: ${ last-question }
 --
